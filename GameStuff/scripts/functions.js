9688e9ee3b196d3e4f6632707692edbb8f57992f
cory <darth22345@hotmail.com>
2018-02-27 20:23:28 +1100

    Added a bunch of stuff

--- GameStuff/scripts/functions.js
+++ GameStuff/scripts/functions.js
@@ -1,12 +1,29 @@
+var attempts = 0;
+
 function login(){
+    var button = document.getElementById('hint');
+    var hint = document.getElementById('output');
     var Password = document.getElementById('Password').value;
     var realPword = '2479778659434212411';
-    if(Password === '2479778659434212411'){
+    
+    if(Password === realPword){
         window.location.href = 'index.html' 
         
     }
 
-    else{
-        document.getElementById('hint').innerHTML = 'Well I wonder what this is: 2479778659434212411';
+    else if(attempts === 3) {
+        hint.innerHTML = 'More than 3 attempts, Jeez I feel bad for you. Heres the  pword: ' + realPword;
+        button.innerHTML = ''
     }
+
+    else{
+        attempts++;
+    }
+}
+
+function hint(){
+    var button = document.getElementById('hint');
+    var hint = document.getElementById('output');
+    button.innerHTML = '';
+    hint.innerHTML = 'Piss off, I don\'t need a hint';
 }
